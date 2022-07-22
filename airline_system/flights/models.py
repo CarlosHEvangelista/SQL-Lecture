4d5d1f68cc10c5c755171ea995e0177b49eb20bf
CarlosHEvangelista <107769306+CarlosHEvangelista@users.noreply.github.com>
2022-07-22 06:50:33 -0300

    Create a index page

--- airline_system/flights/models.py
+++ airline_system/flights/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Airplane(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+
+
+class Flight(models.Model):
+    origin = models.ForeignKey("Airplane", on_delete=models.CASCADE, related_name="Department")
+    destination = models.ForeignKey("Airplane", on_delete=models.CASCADE, related_name="Alternative")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
